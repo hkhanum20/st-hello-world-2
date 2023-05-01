ef770460177656de0cfd68d32e2fa68775b2548f
hkhanum20 <52055072+hkhanum20@users.noreply.github.com>
2023-05-01 13:29:43 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ st.set_page_config(page_title="Hira Khanum's PMM Portfolio", page_icon=":tada:",
 st.subheader("Hi, I am Hira :wave:")
 st.title("Product Marketing Manager")
 st.write("I am a data-driven, “full-stack” Product Marketer in enterprise software. And I am on a mission to move the world forward using technology to make opportunities for growth more accessible.")
-st.write("[Learn More>](https://docs.google.com/presentation/d/1zN2YIOizD9GGoVD_6g4OZdNiEoJbCqgO4ultu9yeWNE/edit?usp=sharing)")
+st.write("[Learn More>](https://docs.google.com/presentation/d/e/2PACX-1vS_XxmF0pVWidAxlSjff5MH4pi123YP-ugvxIS8j5nf_5gtlvlugc88O7EIDdZOJAyfyE7ZKTrqrJlE/pub?start=false&loop=false&delayms=3000)")
 
 st.write("---")
 left_column, right_column=st.columns(2)
@@ -12,7 +12,7 @@ with left_column:
     st.write("##")
     st.write(
         """
-- Ensuring that market realities and only the most important attributes for customer and market adoption influence GTM, I AM AN AMBASSADOR OF INSIGHTS.
+- Ensuring that market realities and only the most important attributes for customer and market adoption influence go-to-market, I AM AN AMBASSADOR OF INSIGHTS.
 - Directing the vision of the go to market plan, I AM A STRATEGIST.
 - Shaping the way the world sees our product and enabling others to tell its story, I AM A STORYTELLER.
 - Connecting insight and action to drive product adoption by shaping market perception through strategic marketing activities, I CREATE LASTING VALUE.
@@ -21,6 +21,40 @@ with left_column:
 
 st.write("---")
 st.header("My Projects")
+st.write("[Messaging](https://docs.google.com/document/d/e/2PACX-1vSEVMPlLOMwteev8iFiHhEbGCCPjMY_iMxDjGzqS2qLTyNE0hPjoFHT6a3x9f-_47a3Al94ATHrQTnA/pub)")
+st.write("[Positioning](https://docs.google.com/document/d/e/2PACX-1vRkKfeYnKeaY3VATxqXwDVYPYibyZXkuE67fEcQDcd39t2Odr2vwwYYVT_p1wwnR2gSOVLQHOjCIW7V/pub)")
+st.write("[Pitch Decks](https://docs.google.com/presentation/d/e/2PACX-1vR3kxsqrqosyGy18phB6RWzZd-vVcOidlFrW3Tmav-URjaXIWvY9SYdaDRcEEI3yQ/pub?start=true&loop=true&delayms=3000)")
+st.write("[Value Proposition Work](https://docs.google.com/spreadsheets/d/e/2PACX-1vR6KzwKJ3X4y4yxmA5gw5IpfYpd5I0BEtzsync3_15xIBicW7zwu5u5HS10QCJbrg/pubhtml)")
+st.write("[Product Roadmaps](https://docs.google.com/spreadsheets/d/e/2PACX-1vTcC4XKMebx2PxupLiC_e_6Re3p0-fwUSXOHB-ooDwnumu-g2EVwEoqsbfcb_pGGA/pubhtml)")
+st.write("[Launch Plans](https://docs.google.com/spreadsheets/d/e/2PACX-1vR3bA5YXmExekQKcZJYW1vyeU41f-XeKfViH1QKpdPt5ja4ufmPe0VVbZMp0Dy5Pm4hh6rRC9zOGVdk/pubhtml)")
+st.write("[Release Plans](https://docs.google.com/presentation/d/e/2PACX-1vR611RRIqRa_jfLq2IXYOofzLFpfB1RqK9IEAvv9arMljeZApNlFMQUiUDSxRpuU-BQhLmYgfWbhFep/pub?start=false&loop=false&delayms=3000)")
+st.write("[Product Councils & QBRs](https://docs.google.com/presentation/d/e/2PACX-1vTQOasSMIiZuAGRPvo5YFuu2wVV_t5TknVp1a_sYICC1wEum7DvxsPUN0L78DeMvQ/pub?start=false&loop=false&delayms=3000)")
+st.write("[Whitepapers & eBooks](https://drive.google.com/file/d/1fFvIDtUa4jhPk0WCT2DKT3dw1QzF8Iuh/view?usp=share_link)")
+st.write("[Analyst Relations](https://docs.google.com/presentation/d/1t6WUhhB47fjbp5Xkbu9YhjZHOx_jMD02/edit?usp=sharing&ouid=103315499357448539628&rtpof=true&sd=true)")
+st.write("[Webinars](https://docs.google.com/presentation/d/e/2PACX-1vTiXkbKgw2PRsPbgCRYCChW5Jdh0D4JNjJIktmzMoGCIpB59L-bZ93wiEEYjIV1Tg/pub?start=false&loop=false&delayms=3000)")
+st.write("[Demos](https://drive.google.com/file/d/1VqsZuiQ_KQ0VHjPXvnRO_nB1hvxvjD9a/view?usp=sharing)")
+st.write("[Communications Briefs](https://docs.google.com/document/d/e/2PACX-1vRl87wO_1098BZgLRDRx4FNoUNEgTTc2fobbsDyrx24ds-QUmOOIxlLN_9u-pQiEw/pub)")
+st.write("[Sales Enablement](https://docs.google.com/presentation/d/1d8RChnXq4QcyjSuJMYttHkrKMoXUwLdn/edit?usp=share_link&ouid=103315499357448539628&rtpof=true&sd=true)")
+st.write("[Sales Trainings](https://docs.google.com/presentation/d/e/2PACX-1vST8LEBAcMozRNBcoUFdNB5-QddKqhsUxsjD3IRkEnyh7H0I5pvwDj14SMqHplSgw/pub?start=false&loop=false&delayms=3000)")
+st.write("[2-Pagers](https://docs.google.com/presentation/d/e/2PACX-1vQbMrpUh2Ay7AWlIfFVsXXUPXoPglF3961HVYr_wjqhNiZL6tMJ8UkVhiyJq4SBbw/pub?start=false&loop=false&delayms=3000)")
+st.write("[1-Pagers](https://drive.google.com/file/d/1neGn0gif-vvrrN-zAQLnHdX7udGilio8/view?usp=sharing)")
+st.write("[Personas & ICPs](https://docs.google.com/presentation/d/e/2PACX-1vRUmKU1zjBDQ7TcF967J4mQY5NQGPYLU_WOypuG6cxmtQTLYNOSLhORh5dw6I8bKg/pub?start=false&loop=false&delayms=3000)")
+st.write("[Case Studies](https://docs.google.com/presentation/d/e/2PACX-1vRTBug5tCa6KUQloqipzKhXC2BLgtA5cJdgotyjlVpD5zMnPWyzRionvqbltdv_fQ/pub?start=false&loop=false&delayms=3000)")
+st.write("[Cheat Sheets](https://docs.google.com/document/d/e/2PACX-1vSXH3x46_zKMSg9bdWqcQe2HnmiDu_9aydD5vBh1irw7AM7zE1Lkxpw0xqh_6gyUA/pub)")
+st.write("[Data Sheets](https://docs.google.com/document/d/e/2PACX-1vRbaaWkn6UYO07b-tJys8a2bjsoZ4oNtQ4WpeHAu9J7bPSdSTX6VuhV2uZ-pa7-HqBFR8ilvcuZCBHk/pub)")
+st.write("[Competition](https://docs.google.com/document/d/e/2PACX-1vQlv5bnBGFVLX28zScm2s5ohMfhDwneZZYh2j5Dc_QoL4CLYqLMnKm-RWHlRoiAvai0TpwQWuO3ID7r/pub)")
+st.write("[Win-Loss Analysis](https://docs.google.com/presentation/d/e/2PACX-1vSeIWY8eBy7gD3uVw_frj_nb6VqeYY08HRI8iD0eJ5_HxtbVt5vTWaHQkvqn3SKxw/pub?start=false&loop=false&delayms=3000)")
+st.write("[Campaigns](https://docs.google.com/presentation/d/e/2PACX-1vSRb4dbYL35r9d5wngq3UuepwasIoq0RCbtEsG_a0FZSMKhr75YF8ZUtivszhn5rFQwb-7GpA8o-pR2/pub?start=false&loop=false&delayms=3000)")
+st.write("[Branding](https://docs.google.com/presentation/d/e/2PACX-1vRPozn5z6V68mHEb3Md2NL8j4Bm9JVlb7HNhpfEmLl3W7X4Wem4iXX1B-kf9MCZtZk8e-7flJtKZ4X4/pub?start=false&loop=false&delayms=3000)")
+st.write("[Content Marketing](https://drive.google.com/file/d/1x59eYyBO1ebI9dr32G-GTrXyIU5tekpV/view)")
+
+video_file = open('C:/Users/hkhanum/Desktop/Gartner D&A Orlando_3.mp4', 'rb') 
+
+video_bytes = video_file.read() 
+
+st.video(video_bytes) 
+
+
 
 
 st.write("---")
@@ -41,3 +75,5 @@ with left_column:
 with right_column:
     st.empty()
     
+
+
